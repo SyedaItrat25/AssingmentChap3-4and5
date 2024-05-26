adf4acd78f48395188956c65ddca2595d38cac73
Syeda Itrat Absar <syedaitrutabsar@gmail.com>
2024-05-26 22:20:38 +0500

    AssingmentChap3,4and5
    Assingment

---
+++ chap 4/script.js
@@ -0,0 +1,28 @@
+///Answer1----------
+let a = 1, b = 2, c = 3;
+console.log(a + b + c);
+/// Answer2---------
+//Legal Variable Names 
+let firstName = "Itrat";
+let _age = 18;
+let $salary = 16000;
+let totalAmount = 14000;
+let user2 = "SyedaItrat";
+
+console.log(firstName + " " + _age + " " + $salary + " " + totalAmount + " " + user2);
+//Illegal Variable Names
+
+// let 2user; 
+// let first-name;
+// let var;
+// let user name;
+// let @home;
+/// Answer3----------
+
+
+
+
+
+
+
+
